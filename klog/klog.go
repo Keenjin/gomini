67d1e8696a647a22329aa55ebe5c8b0c6a2bd7ad
keen <keen@douyu.tv>
2020-08-27 20:20:46 +0800

    klog日志库优化

--- klog/klog.go
+++ klog/klog.go
@@ -6,9 +6,7 @@ var myLogger *klogImpl
 
 // exe的main函数中，使用LogOpen
 func LogOpen(logCfg string) {
-	if myLogger != nil {
-		return
-	}
+	LogClose()
 
 	myLogger = &klogImpl{}
 	myLogger.Open(logCfg)
@@ -18,29 +16,33 @@ func LogOpen(logCfg string) {
 func LogClose() {
 	if myLogger != nil {
 		myLogger.Close()
+		myLogger = nil
+	}
+}
+
+func makesureLoggerValid() {
+	if myLogger == nil {
+		myLogger = &klogImpl{}
+		myLogger.Open("")
 	}
 }
 
 func Debug(format string, args ...interface{}) {
-	if myLogger != nil {
-		myLogger.GetLogger("DEBUG").Output(3, fmt.Sprintf(format, args...))
-	}
+	makesureLoggerValid()
+	myLogger.GetLogger("DEBUG").Output(3, fmt.Sprintf(format, args...))
 }
 
 func Info(format string, args ...interface{}) {
-	if myLogger != nil {
-		myLogger.GetLogger("INFO").Output(3, fmt.Sprintf(format, args...))
-	}
+	makesureLoggerValid()
+	myLogger.GetLogger("INFO").Output(3, fmt.Sprintf(format, args...))
 }
 
 func Warn(format string, args ...interface{}) {
-	if myLogger != nil {
-		myLogger.GetLogger("WARN").Output(3, fmt.Sprintf(format, args...))
-	}
+	makesureLoggerValid()
+	myLogger.GetLogger("WARN").Output(3, fmt.Sprintf(format, args...))
 }
 
 func Error(format string, args ...interface{}) {
-	if myLogger != nil {
-		myLogger.GetLogger("ERROR").Output(3, fmt.Sprintf(format, args...))
-	}
+	makesureLoggerValid()
+	myLogger.GetLogger("ERROR").Output(3, fmt.Sprintf(format, args...))
 }
